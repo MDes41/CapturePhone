47e3331ab4f6433cc378f30c9418a17bfb6b5ff6
Matt D <MDes41@users.noreply.github.com>
2017-03-09 04:00:46 -0700

    Vail and keystone both working, need another api for abasin

--- colorado.js
+++ colorado.js
@@ -29,6 +29,12 @@ export class ColoradoSki extends React.Component {
       }),
       vailCameras: new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2
+      }),
+      keystoneSnow: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      }),
+      keystoneCameras: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
       })
 
 
@@ -36,11 +42,11 @@ export class ColoradoSki extends React.Component {
   }
 
   componentDidMount() {
-    this.getForecastForColorado(1);
-    this.getWebcamForColorado(1);
+    this.getForecastForColorado();
+    this.getWebcamForColorado();
   }
 
-  getForecastForColorado(resort) {
+  getForecastForColorado() {
     return fetch(`https://www.epicmix.com/vailresorts/sites/epicmix/api/mobile/weather.ashx`)
       .then((response) => response.json())
       .then((responseJson) => {
@@ -48,7 +54,8 @@ export class ColoradoSki extends React.Component {
       })
       .then((response) => {
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
-          this.setState({vailSnow: ds.cloneWithRows(response.filter((snow) => snow.resortID === resort))})
+          this.setState({vailSnow: ds.cloneWithRows(response.filter((snow) => snow.resortID === 1))})
+          this.setState({keystoneSnow: ds.cloneWithRows(response.filter((snow) => snow.resortID === 4))})
       })
       .catch((error) => {
         console.error(error);
@@ -56,7 +63,7 @@ export class ColoradoSki extends React.Component {
   }
   
 
-  getWebcamForColorado(resort) {
+  getWebcamForColorado() {
     return fetch(`https://www.epicmix.com/vailresorts/sites/epicmix/api/mobile/mountaincams.ashx`)
       .then((response) => response.json())
       .then((responseJson) => {
@@ -64,7 +71,8 @@ export class ColoradoSki extends React.Component {
       })
       .then((response) => {
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
-          this.setState({vailCameras: ds.cloneWithRows(response.filter((camera) => camera.resortID === resort))})
+          this.setState({vailCameras: ds.cloneWithRows(response.filter((camera) => camera.resortID === 1))})
+          this.setState({keystoneCameras: ds.cloneWithRows(response.filter((camera) => camera.resortID === 4))})
 
       })
       .catch((error) => {
@@ -77,6 +85,8 @@ export class ColoradoSki extends React.Component {
       <ScrollView>
       <Text>Vail, CO</Text> 
       <MountainInfo cameras = {this.state.vailCameras} snow={this.state.vailSnow}/>
+      <Text>Keystone, CO</Text> 
+      <MountainInfo cameras = {this.state.keystoneCameras} snow={this.state.keystoneSnow}/>
       </ScrollView>
     );
   }
